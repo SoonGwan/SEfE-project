20db4b03e2032b80710f9e758b2a0d24bddf448d
yiyb0603 <yiyb0603@naver.com>
2020-02-17 17:06:32 +0900

    [ADD] WorldAirQuality
    세계 대기 질 api 추가

--- src/assets/api/SeaLevelRise/SeaLevelRise.js
+++ src/assets/api/SeaLevelRise/SeaLevelRise.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 class SeaLevelRise {
     async USA_Sea(min, max) {
+        // min 이상 max 이하의 해수면 상승을 가진 바다만 출력
         try {
             const { data } = await axios.get(`https://sea-level-rise-data.herokuapp.com/api/v1/stations?min=${min}&&max=${max}`);
             return data;
---
+++ src/assets/api/WorldAirQuality/WorldAirQuality.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+class WorldAirQuality {
+    async AirQuality(local) {
+        // local 부분에 지역명 써서 받아오기 예) seoul, daegu, gwangju, busan 등등
+        try {
+            const { data } = await axios.get(`https://api.waqi.info/feed/${local}/?token=8c5541d65e7b4ba891b35b3f06e2f884a688abdb`);
+            return data;
+        } catch (error) {
+            throw new Error(`${error}`);
+        }
+    }
+}
+
+export default new WorldAirQuality();

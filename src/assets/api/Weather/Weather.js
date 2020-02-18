fd44155b756de3447d567377d3aaa14e3796b25e
yiyb0603 <yiyb0603@naver.com>
2020-02-18 16:11:50 +0900

    [FIX] 화씨 -> 섭씨
    섭씨로 받아오도록 수정

--- src/assets/api/Weather/Weather.js
+++ src/assets/api/Weather/Weather.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 class Weather {
   async recent(location) {
-    //location 지역 받을꺼임 온도는 화씨로 들어와서 온도 -273 하는 값 가져서 하면됨
+    //location 지역 받을꺼임
     // 1일 요청수가 제한되어있음
     try {
       const { data } = await axios.get(
-        `api.openweathermap.org/data/2.5/weather?q=${location}&appid=c8dd7358613373841407a5221ddc865b`
+        `api.openweathermap.org/data/2.5/weather?q=${location}&appid=c8dd7358613373841407a5221ddc865b&units=metric`
       );
       return data;
     } catch (error) {

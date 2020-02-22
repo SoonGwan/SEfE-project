a1e4643378100dfa26df00f359607b89a8d7322c
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-02-22 15:37:30 +0900

    추가사항

--- src/assets/api/Weather/Weather.js
+++ src/assets/api/Weather/Weather.js
@@ -4,6 +4,7 @@ class Weather {
   async recent(location) {
     //location 지역 받을꺼임
     // 1일 요청수가 제한되어있음
+    // 2월 22일 api 키 변경 되어야 할 수 있음 / 기상청 api를 사용한것도 고려
     try {
       const { data } = await axios.get(
         `api.openweathermap.org/data/2.5/weather?q=${location}&appid=c8dd7358613373841407a5221ddc865b&units=metric`

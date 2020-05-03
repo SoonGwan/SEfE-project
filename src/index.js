f213c07ddb1f549117ec52f7c3677af3da521734
yiyb0603 <yiyb0603@dgsw.hs.kr>
2020-05-03 18:54:35 +0900

    [ADD] store
    store 환경세팅

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
 import App from './components/App';
+import stores from './stores';
+import { Provider } from 'mobx-react';
 import * as serviceWorker from './serviceWorker';
 import 'util/utils.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store ={stores}>
+    <App />
+  </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/stores/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+
+/*
+예시: import Login from './Login';
+
+const stores = {
+  Login: new Login()
+};
+*/
+const stores = {
+    
+};
+
+export default stores;

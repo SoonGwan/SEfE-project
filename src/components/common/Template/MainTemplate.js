0d18ca093f27198466482cd39c66ddb0c09221ff
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-05-03 16:12:04 +0900

    [ADD] Nav, Footer, MainTemplate

---
+++ src/components/common/Template/MainTemplate.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import TopNav from '../TopNav/TopNav';
+import Footer from '../Footer/Footer';
+import PropTypes from 'prop-types';
+
+const MainTemplate = ({ children }) => {
+  return (
+    <>
+      <TopNav />
+      {children}
+      <Footer />
+    </>
+  );
+};
+
+MainTemplate.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.element,
+    PropTypes.object,
+    PropTypes.node,
+  ]).isRequired,
+};
+
+export default MainTemplate;
---
+++ src/components/common/TopNav/TopNav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TopNav.scss';
+
+const TopNav = () => {
+  return (
+    <>
+      <h1 className="TopNav"> 여기는 nav 컴포넌트 입니다.</h1>
+    </>
+  );
+};
+
+export default TopNav;

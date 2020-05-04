cd84be1573dd0b2d612f55e54262c8af84c62e2d
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-05-04 17:57:49 +0900

    [ADD,FIX] 설명란 참조
    App.js : 404 NotFound 처리하였습니다.
    NotFound.js : 404처리를 위한 페이지를 만들었습니다.
    pages : 폴더 위치를 component/Page ==> pages로 이름 변경후 경로를 재설정 하였습니다.
    NotFoundPage.js : NotFound.js컴포넌트를 통일성을 위해 page에 묶어 내보냈습니다.

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,17 @@ import {
   Redirect,
   Router,
 } from 'react-router-dom';
-import MainPage from './Page/MainPage';
+// import * as Pages from '../pages';
+
+import MainPage from 'pages/MainPage';
+import NotFoundPage from 'pages/NotFoundPage';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={MainPage} />
+        <Route path="/notfound" component={NotFoundPage} />
         <Redirect to="/notfound" />
       </Switch>
     </BrowserRouter>

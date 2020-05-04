717594ed0657fd27403d1b1ff5f393f1a3149499
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-05-04 19:34:13 +0900

    [CREATE] NotFoundComponent 퍼블리슁

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import './NotFound.scss';
+import Man10 from 'assets/img/Man10.svg';
+import Woman3 from 'assets/img/Woman3.svg';
+import Man1 from 'assets/img/Man1.svg';
 
 const NotFound = () => {
   return (
     <>
-      <div>NotFound 404</div>
+      <div className="NotFound">
+        <div className="NotFound-wrap">
+          <div className="NotFound-wrap-text">
+            {/* 어라? 길을 잘못 들어선것 같아요! */}
+            어라? 여기는 막다른 길이에요!
+          </div>
+          <div className="NotFound-wrap-notFound">
+            {/* 어라? 길을 잘못 들어선것 같아요! */}
+            Page not Found <b>__</b> <br /> <b>404</b>
+          </div>
+          <img className="NotFound-wrap-img" src={Man1} alt={Man1} />
+        </div>
+      </div>
     </>
   );
 };

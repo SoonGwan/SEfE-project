052100133cc1c2f1118edec1ef5bfa5e935e329a
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-05-10 18:40:05 +0900

    [ADD] NewsTamplate

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,10 +1,29 @@
 import React from 'react';
-
+import './News.scss';
 const News = () => {
   return (
     <>
-      NEWSPAGE
-      <div></div>
+      <div className="News">
+        <div className="News-left">
+          <div className="News-left-newsBox">
+            <div className="News-left-newsBox-img"></div>
+            <div className="News-left-newsBox-side">
+              <div className="News-left-newsBox-title">
+                이곳은 뉴스 타이틀이 들어가는 곳입니다.
+              </div>
+              <div className="News-left-newsBox-subtitle">
+                컬러는 gray2 입니다. 뉴스 내용이 조금 보여지는 곳 입니다 . 다른
+                컨텐츠로 내용 변경 및 수정시 도움이 필요한 경우 아니면 따로 상의
+                없이 수정해도 됩니다.컬러는 gray2 입니다. 뉴스 내용이 조금
+                보여지는 곳 입니다 . 다른 컨텐츠로 내용 변경 및 수정시 도움이
+                필요한 경우 아니면 따로 상의 없이 수정해도 됩니다.
+              </div>
+              <div className="News-left-newsBox-more">More--</div>
+            </div>
+          </div>
+        </div>
+        <div className="News-right"></div>
+      </div>
     </>
   );
 };

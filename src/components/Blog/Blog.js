a05ec8d5ec161ecb8cf16eb962947bb34d1513a6
yiyb0603 <yiyb0603@dgsw.hs.kr>
2020-05-13 18:04:57 +0900

    [ADD] Earth Blog
    Earth Blog 컴포넌트를 추가 및 디자인하였습니다

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,12 +1,52 @@
 /* eslint-disable */
 import React from 'react';
 import './Blog.scss';
+import { FaAngleRight } from 'react-icons/fa';
 
 const Blog = ({ blogList }) => {
-    console.log(blogList);
+    const blogLists = blogList.map((blog, index) => {
+        const { link, description, title } = blog;
+        return (
+            <div className ="Blog-ArticleZone-Article-Item" key ={index}>
+                <div className ="Blog-ArticleZone-Article-Item-Box"></div>
+                <div className ="Blog-ArticleZone-Article-Item-Title">{title.replace(
+                  /<(\/)?([a-zA-Z]*)(\s[a-zA-Z]*=[^>]*)?(\s)*(\/)?>/gi,
+                  ''
+                )
+                .replace(/&quot;/gi, '')}}</div>
+                <div className ="Blog-ArticleZone-Article-Item-description">{description.replace(
+                  /<(\/)?([a-zA-Z]*)(\s[a-zA-Z]*=[^>]*)?(\s)*(\/)?>/gi,
+                  ''
+                )
+                .replace(/&quot;/gi, '')}}</div>
+                <div className ="Blog-ArticleZone-Article-Item-Link">
+                <a href={link} target="_blank" rel ="noopener noreferrer">
+                    More
+                <FaAngleRight
+                  style={{ verticalAlign: 'bottom', fontSize: '20px' }}
+                />
+              </a>
+                </div>
+            </div>
+        );
+    })
     return (
-        <div>
-            Hello
+        <div className ="Blog">
+            <div className ="Blog-Banner">
+                <div className ="Blog-Banner-Top">
+                    <p>This is for Earth</p>
+                    <p>We need your help Lorem Ipsum</p>
+                    <p className ="Blog-Banner-Top-Contents">Lorem IpsumStudy Suggests Rainfall Triggered 2018 Kīlauea Eruption</p>
+                    <button className ="Blog-Banner-Top-Button">More News</button>
+                </div>
+            </div>
+
+            <div className ="Blog-ArticleZone">
+                <div className ="Blog-ArticleZone-Title">Earth Blog</div>
+                <div className ="Blog-ArticleZone-Article">
+                    {blogLists}
+                </div>
+            </div>
         </div>
     );
 }
